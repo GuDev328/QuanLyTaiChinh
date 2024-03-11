179f36fddf1ad522766e009559ccfc9e18266696
GuDev <datminiphi@gmail.com>
2024-03-11 18:21:08 +0700

    built base

---
+++ js/index.js
@@ -0,0 +1,24 @@
+const callApi = (url, method, data) =>
+    new Promise((resolve, reject) => {
+        $.ajax({
+            url: url,
+            type: method,
+            data: JSON.stringify(data),
+            contentType: "application/json",
+            success: function (response) {
+                resolve(response.result.accessToken);
+            },
+            error: function (err) {
+                reject(err.responseJSON.message);
+            },
+        });
+    });
+
+$("#av").click(() => {
+    callApi("http://localhost:3030/users/login", "POST", {
+        email: "datminiphi@gmail.com",
+        password: "5rZd6IQ8eG",
+    })
+        .then(alert)
+        .catch(alert);
+});
