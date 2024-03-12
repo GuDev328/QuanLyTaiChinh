2c2c88e4a49938d39df4218613131108f818975c
GuDev <datminiphi@gmail.com>
2024-03-12 20:22:25 +0700

    base layout

--- js/index.js
+++ js/index.js
@@ -6,7 +6,7 @@ const callApi = (url, method, data) =>
             data: JSON.stringify(data),
             contentType: "application/json",
             success: function (response) {
-                resolve(response.result.accessToken);
+                resolve(response.result);
             },
             error: function (err) {
                 reject(err.responseJSON.message);
@@ -14,11 +14,21 @@ const callApi = (url, method, data) =>
         });
     });
 
-$("#av").click(() => {
-    callApi("http://localhost:3030/users/login", "POST", {
-        email: "datminiphi@gmail.com",
-        password: "5rZd6IQ8eG",
-    })
-        .then(alert)
-        .catch(alert);
+// $("#av").click(() => {
+//     callApi("http://localhost:3030/users/login", "POST", {
+//         email: "datminiphi@gmail.com",
+//         password: "5rZd6IQ8eG",
+//     })
+//         .then(alert)
+//         .catch(alert);
+// });
+
+$(document).ready(function () {
+    $(".item1").click(function () {
+        $(".main-content").load("./pages/menu1.html");
+    });
+
+    $(".item2").click(function () {
+        $(".main-content").load("./pages/menu2.html");
+    });
 });
