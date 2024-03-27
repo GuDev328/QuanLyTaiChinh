5b77eb11937dfd39a0cf0b1461b845d895900254
Lê Quý Độ <leedo.devwork@gmail.com>
2024-03-27 23:09:48 +0700

    some adjust

--- js/script.js
+++ js/script.js
@@ -1,3 +1,28 @@
+const callApi = (url, method, data) =>
+    new Promise((resolve, reject) => {
+        $.ajax({
+            url: url,
+            type: method,
+            data: JSON.stringify(data),
+            contentType: "application/json",
+            success: function (response) {
+                resolve(response.result);
+            },
+            error: function (err) {
+                reject(err.responseJSON.message);
+            },
+        });
+    });
+
+// $("#av").click(() => {
+//     callApi("http://localhost:3030/users/login", "POST", {
+//         email: "datminiphi@gmail.com",
+//         password: "5rZd6IQ8eG",
+//     })
+//         .then(alert)
+//         .catch(alert);
+// });
+
 //Toggle sidebar
 $(document).ready(function () {
     // Toggle sidebar expand class
@@ -60,3 +85,28 @@ sidebarLinks.forEach(link => {
     })
 })
 
+
+var ctx = document.getElementById('myChart').getContext('2d');
+// Khởi tạo dữ liệu cho biểu đồ (Ví dụ: biểu đồ cột)
+var data = {
+    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+    datasets: [{
+        label: 'My First Dataset',
+        backgroundColor: 'rgb(255, 99, 132)',
+        borderColor: 'rgb(255, 99, 132)',
+        data: [65, 59, 80, 81, 56, 55, 40]
+    }]
+};
+
+// Khởi tạo và vẽ biểu đồ
+var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: data,
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
